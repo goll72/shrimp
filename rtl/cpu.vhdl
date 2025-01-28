c04e18e52c9000107e507afff1f0acce6d3bf1e9
goll <joaog.bastos@protonmail.ch>
2025-01-28 20:47:17 -0300

    Oops

--- rtl/cpu.vhdl
+++ rtl/cpu.vhdl
@@ -13,19 +13,5 @@ entity cpu is
 end entity;
 
 architecture structural of cpu is 
-    signal reg1, reg2 : word_t;
 begin
-    registers : entity work.reg_file
-     port map(
-        clk => clk,
-        rst => rst,
-        w_en => '1',
-        w_word => '1',
-        w_addr => (others => '0'),
-        w_data => (others => '1'),
-        reg1_addr => (others => '0'),
-        reg2_addr => "0000" & clk,
-        reg1 => reg1,
-        reg2 => reg2
-    );
 end architecture;
